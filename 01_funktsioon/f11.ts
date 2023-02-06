f513110e7df29988d7f152c4ecfa19f23516ba26
Klaarika Tabbun <114928774+KlaarikaTabbun@users.noreply.github.com>
2023-02-06 16:27:19 +0200

    Add files via upload

---
+++ 01_funktsioon/f11.ts
@@ -0,0 +1,11 @@
+function korrutaKahega(arv: number): number{
+	return arv*2;
+}
+
+//koostage funktsioon kahe arvu keskmise leidmine
+function arvKesk(arv1: number, arv2: number): number{
+	return (arv1 +arv2) /2;
+}
+
+console.log(korrutaKahega(7));
+console.log(arvKesk(8,9));
